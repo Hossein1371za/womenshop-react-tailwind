b6d22556a64ff05646b80de61ca34a4e1f689c24
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-16 23:56:54 +0330

    products

--- src/componenets/product/Products.js
+++ src/componenets/product/Products.js
@@ -8,8 +8,8 @@ const Products = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
         {productData.map((item, index) => {
           return (
-            <div className="flex flex-col items-center gap-y-3 p-4 shadow-xl border-2" key={index}>
-              <img src={item.img} alt="/" />
+            <div className="flex flex-col items-center gap-y-3 p-4 shadow-xl border-2 overflow-hidden" key={index}>
+              <img className="p-6 hover:scale-125 transition-all duration-300" src={item.img} alt="/" />
               <div className="flex items-center justify-between w-full gap-4">
                 <p className="font-semibold text-lg">{item.title}</p>
                 <p className="text-lg number">{item.price}</p>
