4350075a7d92c3e5a3567c20e5aaa89e55837ece
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-18 22:49:14 +0330

    filter

--- src/componenets/dress/FilterProduct.js
+++ src/componenets/dress/FilterProduct.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const FilterProduct = () => {
+  const [range, setRange] = useState(0);
+  console.log(range);
   return (
     <div>
-        <div className='title-filter'>
-            <h3>دسته ها</h3>
-            <p>تی شرت و پولوشرت</p>
-            <p className='border-b-2 pb-4'>پالتو و کاپشن</p>
-        </div>
-        <div className='price-filter'>
-            <h3>قیمت</h3>
-            <input type='range' min="0" max="100" id='range' name='range'/>
-            <label for="range"></label>
-        </div>
+      <div className="title-filter">
+        <h3>دسته ها</h3>
+        <p>تی شرت و پولوشرت</p>
+        <p className="border-b-2 pb-4">پالتو و کاپشن</p>
+      </div>
+      <div className="price-filter">
+        <h3>قیمت</h3>
+        <input
+          onChange={(e) => setRange(e.target.value)}
+          type="range"
+          value="100"
+          min="0"
+          max="100"
+          id="range"
+          name="range"
+        />
+        <label htmlFor="range">{range}</label>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FilterProduct
+export default FilterProduct;
